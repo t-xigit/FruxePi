56af907778a2492d7f6c156d49f9529c592c95bb
Timur <timur@tigitlabs.com>
2020-05-31 17:02:14 +0200

    fix permissions

--- docker/armv7/start.sh
+++ docker/armv7/start.sh
@@ -2,6 +2,10 @@
 
 #FRXPI Container Startup Script
 
+chown www-data:www-data /dev/ttyUSB0
+chown www-data:www-data /var/www/html/assets/tmp/crontab.txt
+chown www-data:www-data /var/www/html/actions/fruxepi.py
+
 # Start CRON
 service cron start
 
